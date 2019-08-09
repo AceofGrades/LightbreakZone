e5afcef3550ee7ca3bd4bb342817908641bc2b6d
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-08-09 19:13:35 +1000

    Added Enemy AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform target;
+
+    private UnityEngine.AI.NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
